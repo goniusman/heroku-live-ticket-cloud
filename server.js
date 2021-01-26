4df5ffde9fc430638608752eaf1a04f10e7bfeba
Osman Goni <goniusman400@gmail.com>
2021-01-26 08:22:45 +0600

    make it better

--- server.js
+++ server.js
@@ -34,7 +34,11 @@ const { static } = require("express")
 app.use('/api/users', userRouter)
 app.use('/api/ticket', ticketRouter)
 
-
+app.get('/', (req, res) => {
+    res.json({
+        message: 'welcomwe to our new server application'
+    })
+})
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
@@ -45,27 +49,17 @@ if(process.env.NODE_ENV === 'production'){
 
 
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'welcomwe to our new server application'
-    })
-})
+
 
 
 
 const PORT = process.env.PORT || 4000
 
 
-app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`)
-
-    // mongoose.connect('mongodb://localhost:27017/ticketing', {useNewUrlParser:true,useUnifiedTopology:true}, () => {
-    // mongoose.connect("mongodb://44447770:44447770@cluster0-shard-00-00.ops0t.mongodb.net:27017,cluster0-shard-00-01.ops0t.mongodb.net:27017,cluster0-shard-00-02.ops0t.mongodb.net:27017/cloud-ticket?ssl=true&retryWrites=true&w=majority&replicaSet=atlas-j60do1-shard-0&authSource=admin", {useNewUrlParser:true, useUnifiedTopology:true}, () => {
+app.listen(PORT, () => {    
        mongoose.connect(`mongodb+srv://44447770:44447770@cluster0.ops0t.mongodb.net/cloud-ticket`, {useNewUrlParser:true, useUnifiedTopology:true}, () => {
           console.log('database connected')
        })
-
-
 })
 
 
